f6a6d81d2951aa11ad3be3d09d0a56113375e34f
Vitor S <svavitor.dev@gmail.com>
2024-04-04 22:47:14 -0300

    chore: add title to cards in django admin

--- carta/models.py
+++ carta/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Carta(models.Model):
     frente = models.TextField(max_length=500)
     verso = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.frente} - {self.verso}'
