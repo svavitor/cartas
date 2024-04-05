7739adac8aa375b0a04c8426c77ad20bd61c50f0
vitor <vitor@yggdrasil.vitor.vms>
2024-04-04 22:36:22 -0300

    feat: add post method to the carta api

--- carta/urls.py
+++ carta/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("list", views.cartas_lista),
-    path("list", views.CartaAPI.as_view(), name="list"),
+    path("", views.CartaAPI.as_view()),    
 ]
--- carta/views.py
+++ carta/views.py
@@ -2,14 +2,18 @@ from carta.models import Carta
 from carta.serializers import CartaSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import HttpResponse
 
 class CartaAPI(APIView):
+    serializer_class = CartaSerializer
+
     def get(self, request):
         lista_cartas = Carta.objects.all()
         serializer = CartaSerializer(lista_cartas, many=True)
         return Response(serializer.data)
-
-def cartas_lista(request):
-    return HttpResponse(status=201)
-
+    
+    def post(self, request):
+        carta = request.data
+        serializer = CartaSerializer(data=carta)
+        if serializer.is_valid(raise_exception=True):
+            carta_saved = serializer.save()
+        return Response({"sucesso"})
