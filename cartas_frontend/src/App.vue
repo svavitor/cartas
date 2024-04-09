0092c5347700273c47324f6bc1127b4b72876683
Vitor S <svavitor.dev@gmail.com>
2024-04-09 12:56:36 -0300

    add flip card css style

--- cartas_frontend/src/App.vue
+++ cartas_frontend/src/App.vue
@@ -26,17 +26,18 @@ onMounted(async () => {
 </script>
 
 <template>
-
   <div>
-      <ul>
-        <li v-for="(card, index) in cards" :key="index">
-        
-          {{ card.front }} <br>
-          {{ card.back }}
-        
-        </li>
-      </ul>
-    </div>
+    <div v-for="(card, index) in cards" :key="index" class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <p>{{ card.front }}</p>
+        </div>
+        <div class="flip-card-back">
+          <p>{{ card.back }}</p>
+        </div>
+      </div>
+    </div> 
+  </div>
   <main>
 
   </main>
@@ -51,6 +52,55 @@ header {
   display: block;
   margin: 0 auto 2rem;
 }
+
+ /* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+ .flip-card {
+  border-radius: 12px;
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  /* border: 1px solid #f1f1f1; */
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  border-radius: 12px;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  border-radius: 12px;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
+} 
 
 @media (min-width: 1024px) {
   header {
