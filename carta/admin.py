bc0c476ef75a37af4552eee308dcf85c288496c1
Vitor S <svavitor.dev@gmail.com>
2024-04-09 09:55:39 -0300

    only god knows what this commit does because i don't

--- carta/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from carta.models import Carta
-
-admin.site.register(Carta)
--- carta/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from carta.models import Carta
-
-class CartaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Carta
-        fields = ['frente','verso', 'next_review']
-
-
